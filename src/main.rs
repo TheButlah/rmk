07b376c5fac597e9bef054b6ced81ed47a062651
Ryan Butler <thebutlah@gmail.com>
2021-12-17 16:09:36 -0500

    Added blinky program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_semihosting as _;
+
+use embedded_hal as hal;
+use nrf52840_hal as c_hal;
+
+use core::num::Wrapping;
+use cortex_m_rt::entry;
+use hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
+
+#[entry]
+fn main() -> ! {
+    let periph = c_hal::pac::Peripherals::take().unwrap();
+    let core_periph = c_hal::pac::CorePeripherals::take().unwrap();
+    let port0 = c_hal::gpio::p0::Parts::new(periph.P0);
+    let mut bled = port0.p0_15.into_push_pull_output(c_hal::gpio::Level::High);
+
+    let mut is_on = true;
+    let mut delay = c_hal::delay::Delay::new(core_periph.SYST);
+    loop {
+        delay.delay_ms(100u16);
+        is_on = !is_on;
+        if is_on {
+            bled.set_high().unwrap();
+        } else {
+            bled.set_low().unwrap();
+        }
+    }
 }
